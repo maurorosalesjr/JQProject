6dde425165b4696c5f8e86e7580bb169f4505e51
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-03-30 18:59:40 -0700

    create toggle function for showing the walrus

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
-jQuery(document).ready(function() {
-  jQuery("h1").click(function() {
-    alert("This is a heading.");
-    alert("I told you, THIS IS A HEADING!");
-  });
+//jQuery(document).ready(function() {
+  //jQuery("h1").click(function() {
+    //alert("This is a heading.");
+    //alert("I told you, THIS IS A HEADING!");
+  //});
 
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
+  //jQuery("p").click(function() {
+    //alert("This is a paragraph.");
+  //});
 
-  jQuery("img").click(function() {
-   alert("This is an image.");
+  //jQuery("img").click(function() {
+   //alert("This is an image.");
+  //});
+//});
+
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
   });
 });
